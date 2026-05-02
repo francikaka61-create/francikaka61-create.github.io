9faffa6edadfcf6e82e7c7ba043b6b1d156d5ed6
francikaka61-create <francikaka61@gmail.com>
2026-05-02 16:24:12 +0300

    Delete capture.php

--- capture.php
+++
@@ -1,8 +0,0 @@
-<?php
-// capture.php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $data = $_POST['data'];
-    $log = "LOG: " . $data . " | IP: " . $_SERVER['REMOTE_ADDR'] . " | TIME: " . date("H:i:s") . PHP_EOL;
-    file_put_contents("accounts.txt", $log, FILE_APPEND);
-}
-?>
